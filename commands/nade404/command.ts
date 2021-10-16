84ac9df06fdc1686947eb39b0d3f247b16c85f7e
b4cktr4ck5r3 <flennertom@gmail.com>
2021-10-16 23:10:45 +0200

    add new command (no logic)

--- commands/nade404/command.ts
+++ commands/nade404/command.ts
@@ -1,25 +1,44 @@
 import { SlashCommandBuilder, SlashCommandStringOption, SlashCommandSubcommandsOnlyBuilder } from "@discordjs/builders";
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, Options } from "discord.js";
 import { nade404handler } from "./handler";
 
 const data : SlashCommandSubcommandsOnlyBuilder = new SlashCommandBuilder()
     .setName("nade404")
     .setDescription("Ask for Nade404 stats")
-    .addSubcommand(subcommandGroup => 
-        subcommandGroup.setName("stats")
-                    .setDescription("Retrieve stats for steam_id")
-                    .addStringOption((options : SlashCommandStringOption) =>
-                        options.setName("steamid")
-                        .setDescription("Player's SteamID")
-                        .setRequired(true))
-    )
-    .addSubcommand(subCommandGroup => 
-        subCommandGroup.setName("top10kd")
-                    .setDescription("Retrieve top 10 players based on kd value")
-    )
-    .addSubcommand(subCommandGroup =>
-        subCommandGroup.setName("top10hs")
-                    .setDescription("Retrieve top 10 players based on headshot percent")
+    .addSubcommand(subCommand =>
+        subCommand.setName("me")
+                .setDescription("Retrieve stats about you if you're registerd"))
+    .addSubcommand(subCommand =>
+        subCommand.setName("register")
+                .setDescription("Register steam_id to disord account")
+                .addStringOption((options: SlashCommandStringOption) =>
+                options.setName("steamid")
+                .setDescription("You're steamid")
+                .setRequired(true)))
+    .addSubcommand(subCommand => 
+        subCommand.setName("top10kd")
+                    .setDescription("Retrieve top 10 players based on kd value"))
+    .addSubcommand(subCommand =>
+        subCommand.setName("top10hs")
+                    .setDescription("Retrieve top 10 players based on headshot percent"))
+    .addSubcommandGroup(subcommandGroup =>
+        subcommandGroup.setName("stats").setDescription("Retrieve stats")
+        .addSubcommand(subCommand => 
+            subCommand.setName("steam")
+                        .setDescription("Retrieve stats for steam_id")
+                        .addStringOption((options : SlashCommandStringOption) =>
+                            options.setName("steamid")
+                            .setDescription("Player's SteamID")
+                            .setRequired(true))
+        )
+        .addSubcommand(subCommand => 
+            subCommand.setName("discord")
+                        .setDescription("Retrieve stats for discord user")
+                        .addStringOption((options : SlashCommandStringOption) =>
+                            options.setName("discord")
+                            .setDescription("Player's Discord")
+                            .setRequired(true))
+        )
     )
 
 const execute = async (interaction : CommandInteraction) => nade404handler(interaction)
--- commands/nade404/handler.ts
+++ commands/nade404/handler.ts
@@ -6,28 +6,49 @@ import { getStatsBySteamId, getTop10Hs, getTop10Kd } from "../../utils/apiCalls"
 import { getErrorTemplate } from "../../utils/template";
 import { getStatsTemplate, getTop10Template } from "./template";
 
+// TODO : upgrade nested switch by replacing with function (better readability)
 export const nade404handler = async (interaction: CommandInteraction) : Promise<void> => {
-    switch (interaction.options.getSubcommand(true)) {
-        case 'stats': {
-            let steamId : string = interaction.options.getString('steamid', true);
-            if (steamId.startsWith("STEAM_0")) steamId = steamId.replace("STEAM_0", "STEAM_1")
+    const subCommandGroup = interaction.options.getSubcommandGroup(false);
+    const subCommand = interaction.options.getSubcommand(false);
 
-            const { success, players } : ApiResponse = await getStatsBySteamId(steamId);
-            if (success) interaction.reply({ embeds: [getStatsTemplate(players as Player)] })
-            else interaction.reply({ embeds: [getErrorTemplate("Error on getting stats", "Cannot get stats")] })
-            break;
+    if (subCommandGroup != null){
+        switch (subCommandGroup) {
+            case 'stats': {
+                switch (subCommand) {
+                    case 'steam': {
+                        let steamId : string = interaction.options.getString('steamid', true);
+                        if (steamId.startsWith("STEAM_0")) steamId = steamId.replace("STEAM_0", "STEAM_1")
+            
+                        const { success, players } : ApiResponse = await getStatsBySteamId(steamId);
+                        if (success) interaction.reply({ embeds: [getStatsTemplate(players as Player)] })
+                        else interaction.reply({ embeds: [getErrorTemplate("Error on getting stats", "Cannot get stats")] })
+                        break;
+                    }
+                    case 'discord': {
+                        interaction.reply("Under development");
+                        break;
+                    }
+                }
+                break;
+            }
         }
-        case 'top10hs': {
-            const { success, players } : ApiResponse = await getTop10Hs();
-            if (success) interaction.reply({ embeds: [getTop10Template(players as Players, Top10Type.HS)] });
-            else interaction.reply({ embeds: [getErrorTemplate("Error on getting top 10 hs", "Cannot get top 10 hs")] })
-            break;
-        }
-        case 'top10kd': {
-            const { success, players } : ApiResponse = await getTop10Kd();
-            if (success) interaction.reply({ embeds: [getTop10Template(players as Players, Top10Type.KD)] });
-            else interaction.reply({ embeds: [getErrorTemplate("Error on getting top 10 K/D", "Cannot get top 10 K/D")] })
-            break;
+    } else {
+        switch (subCommand) {
+            case 'top10hs': {
+                const { success, players } : ApiResponse = await getTop10Hs();
+                if (success) interaction.reply({ embeds: [getTop10Template(players as Players, Top10Type.HS)] });
+                else interaction.reply({ embeds: [getErrorTemplate("Error on getting top 10 hs", "Cannot get top 10 hs")] })
+                break;
+            }
+            case 'top10kd': {
+                const { success, players } : ApiResponse = await getTop10Kd();
+                if (success) interaction.reply({ embeds: [getTop10Template(players as Players, Top10Type.KD)] });
+                else interaction.reply({ embeds: [getErrorTemplate("Error on getting top 10 K/D", "Cannot get top 10 K/D")] })
+                break;            
+            }
+            case 'me' : {
+                interaction.reply(JSON.stringify(interaction.user.username));
+            }
         }
     }
 }
