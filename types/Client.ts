2d182e46573d9727e2250a589f5eb29394707fe1
b4cktr4ck5r3 <flennertom@gmail.com>
2021-10-17 03:05:50 +0200

    upgrade type

--- types/Client.ts
+++ types/Client.ts
@@ -1,10 +1,15 @@
 import { SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "@discordjs/builders";
 import { BaseCommandInteraction, Client } from "discord.js";
 
-export interface Command {
+export interface CommandData {
     data: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder
+}
+
+export interface CommandExec {
     execute (interaction: BaseCommandInteraction): void
 }
+
+export interface Command extends CommandData, CommandExec {}
 
 export interface ClientWithCommands extends Client {
     commands?: Map<string, Command>
--- utils/CommandsFile.ts
+++ utils/CommandsFile.ts
@@ -1,4 +1,4 @@
-import { Command } from "../types/client"
+import { Command, CommandData, CommandExec } from "../types/client"
 import * as fs from 'fs'
 
 const commandsDir : string[] = fs.readdirSync('./commands') // fs is not relative, so ./ works
@@ -6,8 +6,8 @@ const commandsDir : string[] = fs.readdirSync('./commands') // fs is not relativ
 export async function getCommandsAsJSON() {
 	const commandsAsJSON: ReturnType<Command["data"]["toJSON"]>[] = [];
 	for (const dir of commandsDir) {
-		const command : Command = await import(`../commands/${dir}/command.data`); // import is relative, so need ../
-		commandsAsJSON.push(command.data.toJSON());
+		const commandData : CommandData = await import(`../commands/${dir}/command.data`); // import is relative, so need ../
+		commandsAsJSON.push(commandData.data.toJSON());
 	}
 
 	return commandsAsJSON;
@@ -16,8 +16,8 @@ export async function getCommandsAsJSON() {
 export async function getCommandsAsMap() {
 	const commandsAsMap: Map<string, Command> = new Map<string, Command>();
 	for (const dir of commandsDir) {
-		const commandData = await import(`../commands/${dir}/command.data`) // import is relative, so need ../
-		const commandExec = await import(`../commands/${dir}/command.execute`) // import is relative, so need ../
+		const commandData : CommandData = await import(`../commands/${dir}/command.data`) // import is relative, so need ../
+		const commandExec : CommandExec = await import(`../commands/${dir}/command.execute`) // import is relative, so need ../
 		commandsAsMap.set(commandData.data?.name, {...commandData, ...commandExec});
 	}
 
