e9b02b0786bbe139c351d6038fb5060b130d0baa
b4cktr4ck5r3 <flennertom@gmail.com>
2021-10-18 15:25:29 +0200

    update player type for api

--- types/player.ts
+++ types/player.ts
@@ -2,13 +2,20 @@ export interface Player {
     id: number,
     steam_id: string,
     name: string,
+    score: number,
+    rank: number,
+    mvp: number,
     kills: number,
     deaths: number,
     ratio: number,
     headshots: number,
     headshots_percent: number,
     assists: number,
-    flash_assists: number
+    flash_assists: number,
+    no_scope: number,
+    thru_smoke: number,
+    blind: number,
+    wallbang: number
 }
 
 export interface Players {
