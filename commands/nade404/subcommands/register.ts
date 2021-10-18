263a3e56cdc268468361ac2e75f76eb8601934f3
b4cktr4ck5r3 <flennertom@gmail.com>
2021-10-18 17:05:03 +0200

    rename register function

--- commands/nade404/subcommands/register.ts
+++ commands/nade404/subcommands/register.ts
@@ -2,7 +2,7 @@ import { CommandInteraction } from "discord.js";
 import userQuery from "../../../database/userQuery";
 import { FormatSteamId } from "../../../utils/steam";
 
-export async function subCmdRegister(interaction: CommandInteraction) {
+export async function register(interaction: CommandInteraction) {
     const discordId : string = interaction.user.id;
     let steamId : string = FormatSteamId(interaction.options.getString('steamid', true));
 
